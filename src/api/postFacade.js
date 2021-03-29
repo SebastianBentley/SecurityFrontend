0acf203426d50ecc09b33c4db5630b238f83e58c
Sebastian <cph-sb287@cphbusiness.dk>
2021-03-29 15:19:18 +0200

    see all posts

--- src/api/postFacade.js
+++ src/api/postFacade.js
@@ -1,7 +1,19 @@
 import SERVER_URL from "../utils/settings";
 
+
+
+const getToken = () => {
+  return localStorage.getItem('jwtToken')
+}
+const loggedIn = () => {
+  const loggedIn = getToken() != null;
+  return loggedIn;
+}
+
+
+
 function getPosts() {
-  return fetch(SERVER_URL + "/api/posts")
+  return fetch(SERVER_URL + "/api/post/all")
     .then(handleHttpErrors)
     .catch((err) => {
       if (err.status) {
@@ -13,49 +25,50 @@ function getPosts() {
 }
 
 
-function addPost (isbn, title, author, publisher, publishYear){
-  const options = makeOptions("POST", {
-    isbn: isbn,
-    title: title,
-    author: author, 
-    publisher: publisher,
-    publishYear: publishYear
+function addPost(content, username) {
+  const options = makeOptions("POST", true, {
+    username,
+    content
   });
-  return fetch(SERVER_URL+ "/api/post/add-post" ,options)
-  .then(handleHttpErrors)
-  .catch((err) => {
-    if (err.status) {
-      err.fullError.then((e) => console.log(e.message));
-    } else {
-      console.log("Network error");
-    }
-  });
+  return fetch(SERVER_URL + "/api/post/add-post", options)
+    .then(handleHttpErrors)
+    .catch((err) => {
+      console.log(err)
+      if (err.status) {
+        err.fullError.then((e) => console.log(e.message));
+      } else {
+        console.log("Network error");
+      }
+    });
 }
 
 const postFacade = {
-    getPosts,
-    addPost,
-  };
+  getPosts,
+  addPost,
+};
 
-  function makeOptions(method, body) {
-    var opts = {
-      method: method,
-      headers: {
-        "Content-type": "application/json",
-        Accept: "application/json",
-      },
-    };
-    if (body) {
-      opts.body = JSON.stringify(body);
+const makeOptions = (method, addToken, body) => {
+  var opts = {
+    method: method,
+    headers: {
+      "Content-type": "application/json",
+      'Accept': 'application/json'
     }
-    return opts;
   }
-  
-  function handleHttpErrors(res) {
-    if (!res.ok) {
-      return Promise.reject({ status: res.status, fullError: res.json() });
-    }
-    return res.json();
+  if (addToken && loggedIn()) {
+    opts.headers["x-access-token"] = getToken();
   }
-  
-  export default postFacade;
+  if (body) {
+    opts.body = JSON.stringify(body);
+  }
+  return opts;
+}
+
+function handleHttpErrors(res) {
+  if (!res.ok) {
+    return Promise.reject({ status: res.status, fullError: res.json() });
+  }
+  return res.json();
+}
+
+export default postFacade;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,39 +3,61 @@ import React, { useState, useEffect } from "react";
 
 export default function Home() {
 
-    const [dataFromServer, setDataFromServer] = useState([]);
+  const [dataFromServer, setDataFromServer] = useState([]);
+  const [userPost, setUserPost] = useState("");
 
-    useEffect(() => {
-        apiFacade.getPosts().then((data) => {
-           setDataFromServer(data);
-        })
-      }, []);
+  useEffect(() => {
+    apiFacade.getPosts().then((data) => {
+      setDataFromServer(data);
+    })
+  }, [handleSubmit]);
 
-    return (
-      <div className="container-fluid padding">
-        <div className="row">
-          <div className="col-3"></div>
-          <div className="col-6 text-center">
-            <h2 className="mt-5">Forum posts</h2>
-            <hr></hr>
-            <h4 className="mt-5">Login to post</h4>
-            <div class="form-group">
-              <textarea
-                class="form-control"
-                id="exampleFormControlTextarea1"
-                rows="3"
-              ></textarea>
-            </div>
-            <button type="button" class="text-center btn btn-success">Post</button>
-                {dataFromServer && dataFromServer.length > 0 ?
-                dataFromServer.map((m) =>( 
-                    <>
-                    <p>{m.post} {m.userName}</p>
-                    </>
-                )): null}
-            <div className="col-3"></div>
+  function handlePostChange(event) {
+    event.preventDefault();
+    const value = event.target.value;
+    setUserPost(value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    apiFacade.addPost(userPost, localStorage.getItem("username"));
+    setUserPost("");
+  }
+
+  const toShow = dataFromServer ? (
+    <div>
+      {dataFromServer.length > 0 ?
+        dataFromServer.map((m, index) => (
+          <div key={index}>
+            <p>{m.post} {m.username}</p>
           </div>
+        )) : null}
+    </div>
+  ) : ("loading...")
+
+
+  return (
+    <div className="container-fluid padding">
+      <div className="row">
+        <div className="col-3"></div>
+        <div className="col-6 text-center">
+          <h2 className="mt-5">Forum posts</h2>
+          <hr></hr>
+          <h4 className="mt-5">Login to post</h4>
+          <div className="form-group">
+            <textarea
+              className="form-control"
+              id="exampleFormControlTextarea1"
+              rows="3"
+              onChange={handlePostChange}
+              value={userPost}
+            ></textarea>
+          </div>
+          <button type="button" className="text-center btn btn-success" onClick={handleSubmit}>Post</button>
+          {toShow}
+          <div className="col-3"></div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
